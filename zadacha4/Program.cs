f7be94bcaad058e2c15591190e1465fa3362fcdd
Andrey <kadyska21@gmail.com>
2022-08-25 11:37:53 +0300

    задачa4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое число");
+int a = int.Parse(Console.ReadLine());
+int count = 1;
+
+if (a<=1)
+{
+    Console.WriteLine("Вы указали неверное число"); 
+}
+else
+{
+    Console.WriteLine("Четные числа от 1 до N");
+}
+
+while (count <=a)
+{
+    if (count%2==0)
+    {
+        //Console.WriteLine("Четные числа от 1 до N");
+        Console.WriteLine(count );
+    }
+    count = count + 1;
+}
+    
+
+    
